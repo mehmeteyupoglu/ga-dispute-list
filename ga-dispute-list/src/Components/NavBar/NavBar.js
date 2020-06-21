8a8cefa73173a65d5f789ccec38bfe25e3045ff5
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-21 16:44:44 +0300

    Style Navbar

--- ga-dispute-list/src/Components/NavBar/NavBar.js
+++ ga-dispute-list/src/Components/NavBar/NavBar.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, NavbarBrand,} from 'reactstrap';
+
+import Styled from "styled-components"
+
+export const StyledNav = Styled(Navbar)`
+
+background-color: #141417; 
+box-shadow: 0px 2px 45px #141417; 
+img {
+    width: 15%; 
+    height: 15%; 
+}
+
+`
 
 const NavBar = (props) => {
 
     return (
         <div>
-        <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">
-                <img src="https://gamerarena.com/static/img/logo/header-logo-contrast.png" alt="gamer arena"></img>
-            </NavbarBrand>
-        </Navbar>
+        <StyledNav>
+            <Navbar light expand="md" className="NavBar">
+                <NavbarBrand href="/">
+                    <img src="https://gamerarena.com/static/img/logo/header-logo.png" alt="gamer arena"></img>
+                </NavbarBrand>
+            </Navbar>
+        </StyledNav>
         </div>
     );
 }
---
+++ ga-dispute-list/src/Components/NavBar/styles.js
@@ -0,0 +1,13 @@
+import { Navbar } from 'semantic-ui-react'
+import Styled from "styled-components"
+
+export const StyledNav = Styled(Navbar)`
+
+.Navbar {
+    
+}
+
+
+
+
+`
