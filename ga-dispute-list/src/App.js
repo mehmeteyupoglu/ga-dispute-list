8e5905422d53f21f6577f5f2869f16807e69f2a7
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-21 16:57:43 +0300

    Style the mainbody

--- ga-dispute-list/src/App.js
+++ ga-dispute-list/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
 import { Header, Disputes, NavBar } from './Components' 
+import { Container, Row, Col } from 'reactstrap';
+
+import Styled from "styled-components"
+
+export const StyledRow = Styled(Row)`
+padding: 100px; 
+color: white; 
+background-color: #141417; 
+box-shadow: 0px 2px 45px #141417; 
+
+`
 
 
 function App(props) {
   return (
     <div className="App">
       <NavBar />
-      <Header />
-      <Disputes />
+      <Container>
+        <StyledRow>
+          <Row>
+            <Col sm="12" md={{ size: 8, offset: 2 }}>
+              <Header />
+              <Disputes />
+            </Col>
+          </Row>
+        </StyledRow>
+      </Container>
     </div>
   );
 }
