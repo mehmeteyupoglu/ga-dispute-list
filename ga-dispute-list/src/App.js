391264ec94611d3be135f8cc52cb316ae19c3665
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-06-21 16:21:08 +0300

    Configure reactstrap

--- ga-dispute-list/src/App.js
+++ ga-dispute-list/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header, Disputes, NavBar } from './Components' 
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Header />
+      <Disputes />
     </div>
   );
 }
